b973ac9b5da28205dd03b106e48a4243d9ffa9a4
RJ <84078883+elit3rjmg@users.noreply.github.com>
2022-12-25 13:44:28 +0530

    re-structured the code

--- Riddles/Patch 1.2/Riddles v1.2.py
+++ Riddles/Patch 1.2/Riddles v1.2.py
@@ -1,5 +1,49 @@
 import random as rd      
 
+class game:
+    
+    def word(word):
+        match word:
+                case 'What has to be broken before you can use it?':
+                    return 'Hint: Something you eat in breakfast'
+                
+                case 'I am tall when I am young, and I am short when I am old. What am I?':
+                    return 'Hint: You burn it'
+                
+                case 'What is full of holes but still holds water?':
+                    return 'Hint: Who lives in a pineapple and under the sea?'
+                
+                case 'I am an odd number. Take away a letter and I become even. What number am I?':
+                    return 'Hint: no'
+
+                case 'A little girl goes to the store and buys one dozen eggs. As she is going home, all but three break. How many eggs are left unbroken?':
+                    return 'Hint: Odd number'
+
+                case 'If there are three apples and you take away two, how many apples do you have?':
+                    return 'Hint: First Prime number'
+                
+                case 'The more of this there is, the less you see. What is it?':
+                    return 'Hint: Masochist character from Konosuba'
+
+                case 'What is 3/7 chicken, 2/3 cat and 2/4 goat?':
+                    return 'Hint: American state'
+
+                case 'I am a word of letters three; add two and fewer there will be. What word am I?':
+                    return 'Hint: It is very few'
+                
+                case 'What word of five letters has one left when two are removed?':
+                    return 'Hint: The Rock'
+                
+                case 'What word is pronounced the same if you take away four of its five letters?':
+                    return "Hint: It's waiting"
+
+                case 'What is so fragile that saying its name breaks it?':
+                    return "Hint: It's very much quite"
+
+                case  'What can fill a room but takes up no space?':
+                    return "Hint: It's very light"         
+
+
 wordGuess = {
     'What has to be broken before you can use it?' : 'egg',
     'I am tall when I am young, and I am short when I am old. What am I?' : 'candle',
@@ -36,48 +80,10 @@ while count > 0:
         count -= 1
         print('You have',count,'chances')
         print('Try again\n')
-        if count >= 2:
-            
-            match word:
-                case 'What has to be broken before you can use it?':
-                    print('Hint: Something you eat in breakfast')
-                
-                case 'I am tall when I am young, and I am short when I am old. What am I?':
-                    print('Hint: You burn it')
-                
-                case 'What is full of holes but still holds water?':
-                    print('Hint: Who lives in a pineapple and under the sea?')
-                
-                case 'I am an odd number. Take away a letter and I become even. What number am I?':
-                    print('Hint: no')
-
-                case 'A little girl goes to the store and buys one dozen eggs. As she is going home, all but three break. How many eggs are left unbroken?':
-                    print('Hint: Odd number')
-
-                case 'If there are three apples and you take away two, how many apples do you have?':
-                    print('Hint: First Prime number')
-                
-                case 'The more of this there is, the less you see. What is it?':
-                    print('Hint: Masochist character from Konosuba')
-
-                case 'What is 3/7 chicken, 2/3 cat and 2/4 goat?':
-                    print('Hint: American state')
-
-                case 'I am a word of letters three; add two and fewer there will be. What word am I?':
-                    print('Hint: It is very few')
-                
-                case 'What word of five letters has one left when two are removed?':
-                    print('Hint: The Rock')
-                
-                case 'What word is pronounced the same if you take away four of its five letters?':
-                    print("Hint: It's waiting")
-
-                case 'What is so fragile that saying its name breaks it?':
-                    print("Hint: It's very much quite")
-
-                case  'What can fill a room but takes up no space?':
-                    print("Hint: It's very light")                       
         
+        if count >= 2:
+            print(game.word(word))
+                               
         else:
             continue
 
